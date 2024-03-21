cdcee0f7e7ab25d9db8415c095e63cc1d56a706f
Livnath <boug.liv@gmail.comgit config --global user.name Livnathgit config --global user.email boug.liv@gmail.com>
2024-03-21 16:06:37 +0200

    final data

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ def connect(query):
         cursor = conn.cursor()
         cursor.execute(query)
         conn.commit()
-        print('working')
 
     except psycopg2.Error as e:
         print('Error connecting', e)
@@ -25,14 +24,7 @@ def connect(query):
             cursor.close()
             conn.close()
 
-# class user:
-#     def __call__(self,user):
-#         self.user=user
-#     def choice(self):
-#         self.user = input(f'(1) organiser\n(2) volunteer')
-    
-    
-class organiser():
+class organiser:
     def __init__(self,name):
         self.name=name
         
@@ -52,33 +44,32 @@ class organiser():
         prj_skills = input('skill needed (*optional): ')
         prj_job = input ('specific job (*optional): ')
         while True:
-            prj_flight = input('Paying for the flight:\n(1) yes\n(2) no ')
+            prj_flight = input('Paying for the flight:\n(1) yes\n(2) no\n')
             prj_flight=='1'
             if prj_flight == '1' or prj_flight == '2':
                 break
             else:
                 print('Invalid answer')
         while True:
-            prj_house = input('Paying for the house:\n(1) yes\n(2) no')
+            prj_house = input('Paying for the house:\n(1) yes\n(2) no\n')
             if prj_house == '1' or prj_flight == '2':
                 break
             else:
                 print('Invalid answer')
         while True:
-            prj_food = input('Paying for the food during the project:\n(1) yes\n(2) no ')
+            prj_food = input('Paying for the food during the project:\n(1) yes\n(2) no\n')
             if prj_food == '1' or prj_food == '2':
                 break
             else:
                 print('Invalid answer')
         while True:
-            prj_bus = input('Paying for the travel to the project:\n(1) yes\n(2) no ')
+            prj_bus = input('Paying for the travel to the project:\n(1) yes\n(2) no\n')
             if prj_bus == '1' or prj_flight == '2':
                 break
             else:
                 print('Invalid answer')
-        query = f'INSERT INTO project (prj_name, prj_description, prj_country, prj_city, prj_date_start, prj_date_end, prj_skills, prj_job, prj_flight, prj_house, prj_food, prj_bus) VALUES (\'{prj_name}\', \'{prj_description}\',\'{prj_country}\',\'{prj_city}\', \'{prj_date_start}\', \'{prj_date_end}\', \'{prj_skills}\', \'{prj_job}\', \'{prj_flight}\', \'{prj_house}\', \'{prj_food}\', \'{prj_bus}\')'       
+        query = f'INSERT into project (prj_name, prj_description, prj_country, prj_city, prj_date_start, prj_date_end, prj_skills, prj_job, prj_flight, prj_house, prj_food, prj_bus, org_id) VALUES (\'{prj_name}\', \'{prj_description}\',\'{prj_country}\',\'{prj_city}\', \'{prj_date_start}\', \'{prj_date_end}\', \'{prj_skills}\', \'{prj_job}\', \'{prj_flight}\', \'{prj_house}\', \'{prj_food}\', \'{prj_bus}\', (SELECT org_id FROM organiser where org_name = \'{self.name}\'))'       
         connect(query)    
-
 
 class volunteer:
     def __init__(self,name):
@@ -123,8 +114,22 @@ class volunteer:
         query = f'INSERT INTO volunteer (vol_fname, vol_lname, vol_country, vol_city, vol_date_start, vol_date_end, vol_job, vol_skill, vol_phone, vol_mail, vol_flight, vol_house, vol_food, vol_bus) VALUES (\'{vol_fname}\', \'{vol_lname}\',\'{vol_country}\',\'{vol_city}\', \'{vol_date_start}\', \'{vol_date_end}\', \'{vol_job}\', \'{vol_skill}\', \'{vol_phone}\',\'{vol_mail}\', \'{vol_flight}\', \'{vol_house}\', \'{vol_food}\', \'{vol_bus}\')'       
         connect(query)    
 
+class user(organiser,volunteer):
+    def __call__(self,user):
+        self.user=user
+    def choice(self):
+        self.user = int(input(f'(1) organiser\n(2) volunteer\n'))
+        if self.user == 1:
+            self.data_org()
+            self.data_prj()
+        elif self.user == 2:
+            self.vol_data()
+        else:
+            print('invalid answer')
+            
+  
 
-p=volunteer('p')
-p.vol_data()
+p=user('p')
+p.choice()
 
 
